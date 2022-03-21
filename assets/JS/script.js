49aff8d9939d5a199d479b1db6a4570832c3e7b2
BrytanniADJ <brendaevil55@gmail.com>
2022-03-21 20:53:43 -0300

    up

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -6,12 +6,38 @@ const sexo = "B" //declarar letra
 console.log(nome)
 
 const comparaIdade = function (){
-    var idadeMax = 30
+    let idadeMax = 30
+    let op = 1
     if(idade <= idadeMax){
         console.log('idade é menor ', idadeMax)
         console.log(`Idade é menor que ${idadeMax}`)
     }else{
         console.log('Idade é maior' + idadeMax)
     }
+    switch(op) {
+        case 1:
+            console.log('Caso 1')
+            break;
+//caso-escolha
+        case 2:
+            console.log('Caso 2')
+            break;
+//caso não
+        default:
+            console.log('Defalt')
+    }
 }
-comparaIdade()
+
+//função
+const repeticao = function(){
+    for(let i = 0; i < 10; i++){
+        console.log('> ', i)
+    }
+}
+
+//chamar comando
+const inicio = function(){
+    repeticao()
+}
+
+inicio()
