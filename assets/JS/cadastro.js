7ea2da5bdd2c3c040dc78392dad29848c8193ede
BrytanniADJ <brendaevil55@gmail.com>
2022-03-21 22:00:35 -0300

    up

--- assets/JS/cadastro.js
+++ assets/JS/cadastro.js
@@ -2,5 +2,6 @@ const cadastro = function(){
     console.log('> iniciei a função')
     let nome = document.querySelector('#box-nome').value
     let idade = document.querySelector('#box-idade').value
-    console.log(nome, ' - ', idade)
+    let sexo = document.querySelector('#box-sexo').value
+    console.log(nome, ' - ', idade, " - ", sexo)
 }
